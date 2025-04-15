f0cae6822e2ceb784983c1c0bd5dcc37df3c0a33
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-15 09:40:28 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+//primitive
+
+
+//7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+// const score = 100
+// const scoreValue  =  100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+const bigNumber = 78698657364574638n
+
+console.log(typeof bigNumber);
+//Referencs type (Non primitive)
+
+//Array, Object, Functions
+
+const  heros =  ["hiii","kllll","doga"];
+    let myObj = {
+    name:"Raj",
+    age:22,
+
+}
+console.log(typeof heros);
+console.log(typeof myObj);
+
+// const myFunction = function(){
+//       console.log("jhkjdhkds");
+// }
+// myFunction();
+
+// console.log(typeof myFunction()); //undefine
+// console.log(typeof myFunction);
