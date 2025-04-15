4d52b2c4bd4f620664d27789b28feb85edc13513
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-15 06:21:11 +0000

    Conversion of Datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let scores = "33abc"
+
+// console.log(typeof scores);
+// console.log(typeof (scores));
+
+
+// let valueInNumber = Number(scores);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let t = null 
+// let valueInNumbers = Number(t);
+// console.log(typeof valueInNumbers);
+// console.log(valueInNumbers);
+
+// let r = null 
+// let valueInNumb= Number(r);
+// console.log(typeof valueInNumb);
+// console.log(valueInNumb) ;
+
+// let p = true
+// let valueInNumbs= Number(p);
+// console.log(typeof valueInNumbs);
+// console.log(valueInNumbs);
+
+//"33"=>33
+//"33abc"=>33
+//true => 1 ; false=> 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"hioo"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
