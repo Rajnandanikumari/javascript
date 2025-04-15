ff385513502573a5bfe6e59c9408cfe4844149bb
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-15 12:04:14 +0000

    06_nums_and_math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,28 @@
+const score = 400
+
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otN = 23.99967;
+console.log(otN.toPrecision(3));
+
+const hundreds = 100000;
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
