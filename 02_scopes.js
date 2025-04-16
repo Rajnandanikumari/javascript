45e8082fa3f54cf7f006e10295a88c0bb299f30a
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-16 07:33:06 +0000

    Scopes

---
+++ 02_scopes.js
@@ -0,0 +1,49 @@
+// let a  = 10
+// const b = 20
+// var c = 30
+
+// if(true){
+// let a  = 10
+// const b = 20
+// var c = 30
+// }
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
+
+// function one(){
+//     const username = "huuuuu"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+    
+// }
+
+// one()
+
+if(true){
+    const usernames = "nandani"
+    if(usernames === "nandani"){
+        const website = "utube"
+        console.log(usernames +"  "+ website);
+    }
+    // console.log(website);
+}
+
+// console.log(usernames);
+
+function addone(num){
+       return num + 1
+}
+
+console.log(addone(5))
+
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(6));
