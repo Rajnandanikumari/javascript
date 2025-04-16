3cab07cfc73f890a2a0b3c1da3806bbc17b10a9a
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-16 08:43:18 +0000

    Arrowfunction

---
+++ 05_Arrow.js
@@ -0,0 +1,49 @@
+// const user = {
+//     username: "nandani",
+//     price:999,
+
+
+//     welcomeMessage: function()
+//     {
+//         console.log(`${this.username},welcome to website`);
+//         console.log(this);
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "nandani"
+//     console.log(this.username);
+// }
+// chai();
+
+// const chai = () =>{
+//     let username = "sam"
+//     console.log(this);
+// }
+
+// chai();
+
+// const addTwo = (num1,num2) =>  num1 + num2
+
+
+// console.log(addTwo(3,4));
+
+// const addTwo = (num1,num2) => ({username : "shri"})
+
+// console.log(addTwo(3,4))
+
+// const myArray = [2,5,3,7,8]
+// myArray.forEach(() =>{}) 
+
+const addTwo = (num1,num2) => ({username:"raj"})
+console.log(addTwo(3,4))    
+
+const myArray = [2,5,3,7,8]
+
+// myArray.forEach(()=>())
