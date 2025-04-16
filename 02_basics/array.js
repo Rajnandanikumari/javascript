9f8837216425039370b1f7a1d708a01c6228d16a
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-16 04:53:03 +0000

    Array

---
+++ 02_basics/array.js
@@ -0,0 +1,42 @@
+//array
+
+// const myArray =  [0,1,2,3,4,5,true,7]
+// console.log(myArray["one"]);
+
+// const myHeros = ["Sam","Kam"]
+const myArr = new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+// myArr.push(6)
+// myArr.push(7)
+// // myArr.pop()
+// console.log(myArr);
+// myArr.unshift(0);
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(0));
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(newArr);
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+// console.log("B",myArr);
+
+//slice,splice
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B",myArr);
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+
