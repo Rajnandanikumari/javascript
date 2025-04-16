fda551133601285f68177fe4a0ea61a13614da81
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-16 11:46:49 +0000

    BasictoBegginer

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+//if
+
+// const isUserloggedIn = true
+// const temp = 41
+// if(temp === 41){
+//      console.log("less than 50");
+// }else{
+// console.log("temp is greater than 50");
+// }
+
+// const score = 200
+// if(score > 100){
+//      let power = "fly"
+//      console.log(`User power : ${power}`);
+// }
+// console.log(`User power : ${power}`);
+
+// const bal = 1000
+// if(bal>500)console.log("test"),console.log("test2");❌
+
+// const bal = 1000;
+// if(bal<500){
+//     console.log("less than 500");
+// }else if(bal < 750){
+//    console.log("less than 750");
+// }
+// else if(bal<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+
+// if(userLoggedIn && debitCard && 2==3){
+//     console.log("Allow to buy course");
+// }
+const loggedInFromGoogle = false
+const loggedInFromemail = true
+if(loggedInFromGoogle || loggedInFromemail){
+    console.log("User logged in");
+}
