a9169a8048a5f31ec3cc28ff58f1f96c951d9d87
Codeco <118913721+Rajnandanikumari@users.noreply.github.com>
2025-04-16 12:55:37 +0000

    iteration

---
+++ 05_iterations/one.js
@@ -0,0 +1,42 @@
+//for
+
+// for(let i = 0;i<=10;i++){
+//     const e = i;
+//     if(e == 5){
+//         console.log("5 is best number");
+//     }
+
+// console.log(e);
+// }
+
+// for(let i = 0;i<=10;i++){
+//     console.log(`Outer loop value : ${i}`);
+//     for(let j = 0 ;j<=10;j++){
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+// }
+
+// let myArray = ["flash","batman","superman"]
+
+// for(let i=0;i<myArray.length;i++){
+//     const e = myArray[i];
+//     console.log(e);
+// }
+
+//break and continue
+
+// for(let i=1;i<=20;i++){
+//     if(i == 5){
+//         console.log(`Detected 5`)
+//         break
+//     }
+//     console.log(`Value of i is ${i}`);
+// }
+
+for(let i=1;i<=20;i++){
+    if(i == 5){
+        console.log(`Detected 5`)
+        continue
+    }
+    console.log(`Value of i is ${i}`);
+}
